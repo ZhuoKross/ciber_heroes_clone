4bcd72c76012b0b783828ca45f40d3bf819f8c11
Michael Garnica <mgarnica.oficial@gmail.com>
2024-11-17 21:31:01 -0500

    Redoing the first map, and adding more interactable objects inside map

--- src/service/initGame.js
+++ src/service/initGame.js
@@ -35,21 +35,21 @@ export default async function initGame(canvas) {
                     loop: true,
                     speed: 5
                 },
-                "walk-left":{
+                "walk-left": {
                     from: 8,
                     to: 11,
                     loop: true,
                     speed: 5
                 },
-                "walk-down":{
+                "walk-down": {
                     from: 12,
                     to: 15,
                     loop: true,
                     speed: 5
                 },
-                "walk-up":{
+                "walk-up": {
                     from: 16,
-                    to: 19 ,
+                    to: 19,
                     loop: true,
                     speed: 5
                 }
@@ -63,14 +63,14 @@ export default async function initGame(canvas) {
     }
 
     try {
-        await k.loadSprite("map", "/assets/ciber_heroes.png");
+        await k.loadSprite("map", "/assets/map_01_ciber_heroes.png");
     } catch (error) {
         console.log("Error uploading the map: ", error);
     }
 
 
 
-    const mapData = await fetch("/assets/ciber_heroes.json").then(res => res.json())
+    const mapData = await fetch("/assets/map_01_ciber_heroes.json").then(res => res.json())
 
 
 
@@ -87,7 +87,7 @@ export default async function initGame(canvas) {
 
         const map = k.add([
             k.pos(100, 0),
-            k.scale(3),
+            k.scale(2),
             k.sprite("map")
         ]);
 
@@ -100,7 +100,7 @@ export default async function initGame(canvas) {
             k.body(),
             k.anchor("center"),
             k.pos(),
-            k.scale(2),
+            k.scale(3),
             {
                 speed: 200,
                 direction: "left",
@@ -118,7 +118,20 @@ export default async function initGame(canvas) {
 
             //console.log(layer);
 
-            
+            if(layer.name === "limits" ){
+                for (const obj of layer.objects){
+                    
+                    console.log(`Creando objeto: ${obj.name}`)
+
+                    map.add([
+                        k.area({ shape: new k.Rect(k.vec2(0), obj.width, obj.height) }),
+                        k.body({ isStatic: true }),
+                        k.pos(obj.x, obj.y),
+                        obj.name
+                    ])
+                }
+            }
+
 
 
             const objects = [];
@@ -138,31 +151,7 @@ export default async function initGame(canvas) {
                         obj.name
                     ])
 
-                    if(obj.name === "chair_01" || obj.name === "chair_02" || obj.name === "computer"){
-                        k.onCollide("player", obj.name, () => {
-                    
-                            console.log(`collision with object: ${obj.name}`)
-
-                            
-                            player.isOnDialogue = true;
-            
-                            displayDialogue("CUALQUIER TEXTO", () => {
-                                player.isOnDialogue = false
-                            });
-                        })
-                    }else{
-                        continue
-                    }
-
-                }   
-
-            }
-
-            //console.log("Objetos que colisionan: ",objects);
-
-            if (layer.name === "positions") {
-                console.log("capa procesando: ", layer.name)
-                for (const obj of layer.objects) {
+                    // FOR THE POSITION SPAWN OF THE PLAYER
                     if (obj.name === "player") {
                         player.pos = k.vec2(
                             (map.pos.x + obj.x + 50),
@@ -173,75 +162,93 @@ export default async function initGame(canvas) {
 
 
                     }
-                }
-            }
 
+                    if (obj.name === "info") {
+                        k.onCollide("player", obj.name, () => {
+
+                            console.log(`collision with object: ${obj.name}`)
+
+
+                            player.isOnDialogue = true;
+
+                            displayDialogue("CUALQUIER TEXTO", () => {
+                                player.isOnDialogue = false
+                            });
+                        })
+                    } else {
+                        continue
+                    }
+
+                }
+
+            }
         }
 
 
-        
-        k.onUpdate(() => {
-            k.camPos(player.pos.x, player.pos.y + 100);
-        })
-        
-        
-        k.onMouseDown((mouseBtn) => {
-            if(mouseBtn !== "left" || player.isOnDialogue) return;
-            
-            const worldMousePosition = k.toWorld(k.mousePos());
-            player.moveTo(worldMousePosition, player.speed);
-        })
-        
-        k.onKeyDown("a", ()=>{
-            player.move(-SPEED, 0)
-            if(player.getCurAnim().name !== "walk-left"){
-                //console.log("name of the current animation:", player.getCurAnim().name)
-                player.play("walk-left")
-            }
-        })
-        
-        k.onKeyDown("w", () => {
-            player.move(0, -SPEED)
 
-            if(player.getCurAnim().name !== "walk-up"){
-                //console.log("name of the current animation:", player.getCurAnim().name)
-                player.play("walk-up")
-            }
-        });
-        
-        k.onKeyDown("s", ()=> {
-            player.move(0, SPEED)
 
-            if(player.getCurAnim().name !== "walk-down"){
-                //console.log("name of the current animation:", player.getCurAnim().name)
-                player.play("walk-down")
-            }
-        })
+            k.onUpdate(() => {
+                k.camPos(player.pos.x, player.pos.y + 100);
+            })
 
-        k.onKeyDown("d", ()=> {
-            player.move(SPEED, 0)
 
-            if(player.getCurAnim().name !== "walk-right"){
-                //console.log("name of the current animation:", player.getCurAnim().name)
-                player.play("walk-right")
-            }
-        })
-        
-        const keys = ["w", "a", "s", "d"];
-        
-        keys.forEach(key => {
-            k.onKeyRelease(key, ()=>{
-                if(!k.isKeyDown("w") || !k.isKeyDown("a") || k.isKeyDown("s") || k.isKeyDown("d")){
-                    player.play("idle");
+            k.onMouseDown((mouseBtn) => {
+                if (mouseBtn !== "left" || player.isOnDialogue) return;
+
+                const worldMousePosition = k.toWorld(k.mousePos());
+                player.moveTo(worldMousePosition, player.speed);
+            })
+
+            k.onKeyDown("a", () => {
+                player.move(-SPEED, 0)
+                if (player.getCurAnim().name !== "walk-left") {
+                    //console.log("name of the current animation:", player.getCurAnim().name)
+                    player.play("walk-left")
                 }
             })
-        });
-        
-        // ANIMATIONS AND LOGIC FOR TRIGGER IT 
-        player.play("idle");
-    
-        
-    })
+
+            k.onKeyDown("w", () => {
+                player.move(0, -SPEED)
+
+                if (player.getCurAnim().name !== "walk-up") {
+                    //console.log("name of the current animation:", player.getCurAnim().name)
+                    player.play("walk-up")
+                }
+            });
+
+            k.onKeyDown("s", () => {
+                player.move(0, SPEED)
+
+                if (player.getCurAnim().name !== "walk-down") {
+                    //console.log("name of the current animation:", player.getCurAnim().name)
+                    player.play("walk-down")
+                }
+            })
+
+            k.onKeyDown("d", () => {
+                player.move(SPEED, 0)
+
+                if (player.getCurAnim().name !== "walk-right") {
+                    //console.log("name of the current animation:", player.getCurAnim().name)
+                    player.play("walk-right")
+                }
+            })
+
+            const keys = ["w", "a", "s", "d"];
+
+            keys.forEach(key => {
+                k.onKeyRelease(key, () => {
+                    if (!k.isKeyDown("w") || !k.isKeyDown("a") || k.isKeyDown("s") || k.isKeyDown("d")) {
+                        player.play("idle");
+                    }
+                })
+            });
+
+            // ANIMATIONS AND LOGIC FOR TRIGGER IT 
+            player.play("idle");
+
+
+        })
 
 
     k.go("game");
