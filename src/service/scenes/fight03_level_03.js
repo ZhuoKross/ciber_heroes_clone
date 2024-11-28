52d0a868d9bcada30ee82b187ac646cd99ae9cf4
Michael Garnica <mgarnica.oficial@gmail.com>
2024-11-28 03:13:37 -0500

    changes of third scene

--- src/service/scenes/fight03_level_03.js
+++ src/service/scenes/fight03_level_03.js
@@ -1,5 +1,5 @@
 import dialogFigth from "../dialogFigth";
-export default async function fightThreeLevelThree(k, goBackScene){
+export default async function fightThreeLevelThree(k, goBackScene) {
     // k.add([
     //     k.text("fight 03 level 3"),
     //     k.pos(100, 100)
@@ -15,48 +15,49 @@ export default async function fightThreeLevelThree(k, goBackScene){
         k.scale(1.7, 0.8)
     ])
 
-        // k.add([
-        //     k.rect(canvasWidth, 200),
-        //     k.pos(0, canvasHeight - 50),
-        //     k.area(),
-        //     k.body({isStatic: true}),
-        //     k.color(k.Color.fromHex(("#020232")))
-        // ]);
-        function introDialogue() {
+    k.add([
+        k.rect(canvasWidth, 200),
+        k.pos(0, canvasHeight - 50),
+        k.area(),
+        k.body({ isStatic: true }),
+        k.color(k.Color.fromHex(("#020232")))
+    ]);
 
-            player.isOnDialogue = true;
-            console.log("the player is in dialogue? ", player.isOnDialogue);
-            const resp = "b. Un virus que afecta el funcionamiento de tu dispositivo";
-            dialogFigth(
-                k,
-                "¿Cuál de los siguientes es un ejemplo de 'malware'?",
-                ["a. Una aplicación de banca segura", "b. Un virus que afecta el funcionamiento de tu dispositivo", "c.	Un sistema operativo actualizado", "d. Un archivo adjunto en un correo de un desconocido."],
-                k.vec2(canvasWidth / 2, canvasHeight / 2),
-                (selectedOption) => {
-                    console.log("Opción seleccionada:", selectedOption);
-                    if(selectedOption === resp){
-                        k.setGravity(null)
-                        goBackScene()
-                    }else{
-                        alert("lastima sapa")
-                    }
-                },
-                () => {
-                    console.log("Diálogo cerrado");
-                    // Lógica adicional si el diálogo se cierra sin enviar
+    function introDialogue() {
+
+        player.isOnDialogue = true;
+        console.log("the player is in dialogue? ", player.isOnDialogue);
+        const resp = "b. Un virus que afecta el funcionamiento de tu dispositivo";
+        dialogFigth(
+            k,
+            "¿Cuál de los siguientes es un ejemplo de 'malware'?",
+            ["a. Una aplicación de banca segura", "b. Un virus que afecta el funcionamiento de tu dispositivo", "c.	Un sistema operativo actualizado", "d. Un archivo adjunto en un correo de un desconocido."],
+            k.vec2(canvasWidth / 2, canvasHeight / 2),
+            (selectedOption) => {
+                console.log("Opción seleccionada:", selectedOption);
+                if (selectedOption === resp) {
+                    k.setGravity(null)
+                    goBackScene()
+                } else {
+                    alert("lastima sapa")
                 }
-            );
-             }
-             
+            },
+            () => {
+                console.log("Diálogo cerrado");
+                // Lógica adicional si el diálogo se cierra sin enviar
+            }
+        );
+    }
+
 
     const boss03 = k.add([
         k.sprite("third_boss_level_03"),
-        k.pos(300, canvasHeight - 400),
+        k.pos(200, canvasHeight - 50),
         k.body(),
-        k.area({shape: new k.Rect(k.vec2(0), 25, 180)}),
+        k.area({ shape: new k.Rect(k.vec2(0), 25, 180) }),
         k.anchor("center"),
         k.scale(3),
-        {anim: "idle"}
+        { anim: "idle" }
     ])
 
     let player = k.add([
@@ -67,7 +68,7 @@ export default async function fightThreeLevelThree(k, goBackScene){
         }),
         k.body(),
         k.anchor("center"),
-        k.pos(canvasWidth - 100, canvasHeight - 100),
+        k.pos(canvasWidth - 100, canvasHeight - 50),
         k.scale(6),
         {
             speed: 200,
@@ -84,9 +85,9 @@ export default async function fightThreeLevelThree(k, goBackScene){
     boss03.play("idle");
     player.play("idle");
 
-    k.onKeyPress("u", () =>{
+    k.onKeyPress("u", () => {
         goBackScene()
     })
-    
+
     introDialogue();
 }
